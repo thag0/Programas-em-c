eca17a1fb8c6d0bf696e14fcaebe0b8de96f999c
Thiago <91092364+thag0@users.noreply.github.com>
2023-05-14 00:43:27 -0300

    Add files via upload

--- paralelizacao_numeros_primos.c
+++ paralelizacao_numeros_primos.c
@@ -21,25 +21,28 @@ void* calcular_numeros_primos(void* args){
 
    Dados* dados = (Dados*) args;
    long* vetor_numeros = dados->vetor_numeros;
+   long inicio = dados->indice_inicio;
+   long fim = dados->indice_fim;
 
    /*
       cada thread fica responsavel pelo mesmo vetor
       mas vai trabalhar entre um certo intervalo de indices
       como se fossem mini vetores virtuais
    */
-   for(long indice = dados->indice_inicio; indice < dados->indice_fim; indice++){
+   for(long indice = inicio; indice < fim; indice++){
       divisoes = 0;
       valor = vetor_numeros[indice];
+
       for(long i = 1; i <= valor; i++){
          if(valor % i == 0) divisoes ++;
       }
+
       if(divisoes == 2){
          pthread_mutex_lock(&mutex);
          (*dados->numeros_primos) += 1;
          pthread_mutex_unlock(&mutex);
       }
    }
-
 }
 
 
@@ -57,8 +60,8 @@ int main(){
    Dados* dados = (Dados*) malloc(NUM_THREADS * sizeof(Dados));
    long indice_inicio = 0;
    long indice_fim = (long) tamanho_vetor/NUM_THREADS;
+   
    pthread_mutex_init(&mutex, NULL);
-
    for(int i = 0; i < NUM_THREADS; i++){
       dados[i].indice_inicio = indice_inicio;
       dados[i].indice_fim = indice_inicio + indice_fim;
@@ -73,9 +76,13 @@ int main(){
    for(int i = 0; i < NUM_THREADS; i++){
       pthread_join(threads[i], NULL);
    }
-
    pthread_mutex_destroy(&mutex);
+
    printf("Numeros primos entre %ld e %ld: %ld\n", vetor_numeros[0], vetor_numeros[tamanho_vetor-1],*numeros_primos);
+
+   free(dados);
+   free(vetor_numeros);
+   free(numeros_primos);
 
    return 0;
 }
