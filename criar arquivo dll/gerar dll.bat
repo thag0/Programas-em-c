a783508d99797dca513483a4403a066458717174
Thiago <91092364+thag0@users.noreply.github.com>
2023-05-16 20:07:19 -0300

    Delete criar arquivo dll directory

--- criar arquivo dll/gerar dll.bat
+++
@@ -1,9 +0,0 @@
-cls
-set /p NOMEARQUIVO=Digite o nome do arquivo:
-
-gcc -c %NOMEARQUIVO%.c -o %NOMEARQUIVO%.o
-gcc -shared -o %NOMEARQUIVO%.dll %NOMEARQUIVO%.o
-
-cls
-@echo compilacao finalizada.
-@pause
